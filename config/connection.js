f69591ab86bac4f2ffb7550311be9e22e882402c
surajgurung <microbiosuraj@gmail.com>
2020-05-31 08:28:48 -0400

    deploy

--- config/connection.js
+++ config/connection.js
@@ -2,11 +2,11 @@
 var mysql = require("mysql");
 
 var connection = mysql.createConnection({
-  host: "pqxt96p7ysz6rn1f.cbetxkdyhwsb.us-east-1.rds.amazonaws.com",
+  host: "localhost",
   port: 3306,
-  user: "wtwrv4d87yy9b5zb",
-  password: "m3dv2s5bgm791pi5",
-  database: "lxoa3h0dqiak9c4p"
+  user: "root",
+  password: "rootpass",
+  database: "burgers_db"
 });
 
 // Make connection.
